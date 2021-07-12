aae14d2f0f21d6f57219bd8b66c503566401aaa4
fddf Gbljhfcs <symba82@yandex.ru>
2021-07-12 23:20:00 +0300

    Lectur3

--- Bugakov_lecture_3/Program.cs
+++ Bugakov_lecture_3/Program.cs
@@ -4,9 +4,48 @@ namespace Bugakov_lecture_3
 {
     class Program
     {
+        struct Complex
+        {
+            public double im;
+            public double re;
+            //  в C# в структурах могут храниться также действия над данными
+            public Complex Plus(Complex x)
+            {
+                Complex y;
+                y.im = im + x.im;
+                y.re = re + x.re;
+                return y;
+            }
+            //  Пример произведения двух комплексных чисел
+            public Complex Multi(Complex x)
+            {
+                Complex y;
+                y.im = re * x.im + im * x.re;
+                y.re = re * x.re - im * x.im;
+                return y;
+            }
+            public string ToString()
+            {
+                return re + "+" + im + "i";
+            }
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Complex complex1;
+            complex1.re = 1;
+            complex1.im = 1;
+
+            Complex complex2;
+            complex2.re = 2;
+            complex2.im = 2;
+
+            Complex result = complex1.Plus(complex2);
+            Console.WriteLine(result.ToString());
+            result = complex1.Multi(complex2);
+            Console.WriteLine(result.ToString());
+
         }
     }
 }
